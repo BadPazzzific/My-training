c0dd4cf8eedee96bc8757fd6981b06afeb71b388
BadPazzzific <BlackRock55@yandex.ru>
2024-08-21 19:48:22 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,11 @@
+immutable_var = 1, 'Hello', False, 2.3
+tuple_ = immutable_var
+print(immutable_var)
+mutable_list = ([1,2,3,4])
+print(mutable_list)
+mutable_list [0]=10
+mutable_list [1]=20
+mutable_list [2]=30
+mutable_list [3]=40
+print(mutable_list)
+
