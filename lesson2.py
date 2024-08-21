160f8ba6ee6163f1c5b09cccfda98f453a9e31cc
BadPazzzific <BlackRock55@yandex.ru>
2024-08-21 19:57:52 +0300

    Add files via upload

---
+++ lesson2.py
@@ -0,0 +1,5 @@
+completed = 12                          # Количество выполненных ДЗ
+spent = 1.5                             # Количество затраченных часов
+average_time = (spent/completed)        # Время на одно задание
+name_kurse = 'Python'                   # Название курса
+print (name_kurse,',' 'всего задач'':', completed, 'затрачено часов'':', spent, 'среднее время выполнения'':', average_time)
